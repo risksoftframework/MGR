3955e43897d2990f282e6822537642eafa0710f9
risksoftframework <97638936+risksoftframework@users.noreply.github.com>
2022-01-12 19:00:30 -0500

    Guía Electrónica de Procesos de la MGR

---
+++ scripts/ContentPageResource.js
@@ -0,0 +1,36 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2000, 2007 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+/* resource files for content page javascript 
+ * this file should be localized
+*/
+ContentPageResource = function() {
+
+
+this.expandAllText = "Expandir todas las seccciones";
+this.collapseAllText = "Contraer todas las seccciones";
+this.backToTopText = "Volver al inicio";
+
+this.expandAllSubSectionsText = "Expandir todos los pasos";
+this.collapseAllSubSectionsText = "Contraer todos los pasos";
+
+this.toolbar_bookmark = "Bookmark this page";
+this.toolbar_back = "Back";
+this.toolbar_forward = "Forward";
+this.toolbar_expand_all = "Expand all";
+this.toolbar_collapse_all = "Collapse all";
+this.toolbar_print = "Print";
+
+this.expandText = "Expand"
+this.collapseText = "Collapse"
+this.checkedAltText = "Yes"	
+
+};
